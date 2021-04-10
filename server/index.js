295c21a79dd1adce798e8dd8dd2fa54b74329e4f
Jorgen <ybeliaev@gmail.com>
2021-04-10 13:53:59 +0300

    base for controllers

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use('./api', router)
+app.use('/api', router)
 
 // app.get('/', (req,res)=>{
 //   res.status(200).json({message: "It's working!!!"})
