45541de84122a29ea289729aca59620416d8ed72
Jorgen <ybeliaev@gmail.com>
2021-04-10 12:54:09 +0300

    add router

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # React & Node.js Fullstack интернет магазин.
 
 PERN stack: PostgreSQL + express + React + node.js
+
+## 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,21 @@
 require('dotenv').config()
+
 const express = require('express')
 const sequelize = require('./db')
-
+const models = require('./models/models')
+const cors = require('cors')
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('./api', router)
+
+// app.get('/', (req,res)=>{
+//   res.status(200).json({message: "It's working!!!"})
+// })
 
 const start = async () =>  {
   try {
